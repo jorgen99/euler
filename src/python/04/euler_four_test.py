0dd5849c2026b0fc09e5ae8932483a259b699565
Jörgen Lundberg <jorgen.lundberg@gmail.com>
2019-07-16 00:42:19 +0200

    Typo in test for Euler 04

--- src/python/04/euler_four_test.py
+++ src/python/04/euler_four_test.py
@@ -6,24 +6,24 @@ from euler_four import EulerFour
 class EulerFourTest(unittest.TestCase):
 
     def setUp(self):
-        self.euler_three = EulerFour()
+        self.euler_four = EulerFour()
 
     def test_is_palindrome(self):
-        is_palindrome = self.euler_three.is_palindrome(1001)
+        is_palindrome = self.euler_four.is_palindrome(1001)
         assert_that(is_palindrome, is_(True))
 
-        is_palindrome = self.euler_three.is_palindrome(9009)
+        is_palindrome = self.euler_four.is_palindrome(9009)
         assert_that(is_palindrome, is_(True))
 
-        is_palindrome = self.euler_three.is_palindrome(202)
+        is_palindrome = self.euler_four.is_palindrome(202)
         assert_that(is_palindrome, is_(True))
 
     def test_2_digit_numbers(self):
-        palindrome = self.euler_three.max_palindromic(2)
+        palindrome = self.euler_four.max_palindromic(2)
         assert_that(palindrome, is_(9009))
 
     def test_3_digit_numbers(self):
-        palindrome = self.euler_three.max_palindromic(3)
+        palindrome = self.euler_four.max_palindromic(3)
         assert_that(palindrome, is_(906609))
 
 
